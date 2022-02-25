65a64b938ab4ec2068f183a31e64c48d31985607
Jim Riddle <jimriddle0@gmail.com>
2022-02-24 20:54:15 -0600

    Add annotations

--- variables.rb
+++ variables.rb
@@ -1,15 +1,19 @@
+#This is where I put in the strings, 3 of them to be exact
 name = "Jim Riddle"
 address = "somewhere in Chicago"
 favorite_food = "good juicy burger"
 
+#this is where I put in some integers
 age = 31
 height = 71
 weight = 145
 
+#this is where i put in some floats
 bank_account = 0.45
 height_feet = 5.9
 perscription = 0.25
 
+#this is where I put in some booleans
 lights_on = true
 is_hungry = false
 is_happy = true
